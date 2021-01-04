a10f12bfe38e53e06ec47bd19eaceadb61e776de
Rashid <rashidkisome@gmail.com>
2021-01-04 18:49:33 +0100

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { MainViewComponent } from './pages/main-view/main-view.component';
 import {DragDropModule} from "@angular/cdk/drag-drop";
 
-
+ /* */
 @NgModule({
   declarations: [
     AppComponent,
